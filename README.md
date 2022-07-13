a9ff8effe2515de601f8094ac5c6772558bae2cf
AbimaelP <92121999+AbimaelP@users.noreply.github.com>
2022-07-13 13:37:04 -0300

    commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# app-manual-search
+# leitor_arquivos
+ app criado para ajudar nas tarefas repetitivas em sites web
---
+++ debug.js
@@ -0,0 +1,8 @@
+let texto = "O rato roeu a roupa do rei de roma com aroma de romã";
+function showTxt(file) {
+  text = file;
+  //text = JSON.stringify(text)
+  let formatado = text.split(' ');
+  console.log(formatado)
+}
+showTxt(texto)
